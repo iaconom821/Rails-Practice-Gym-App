fbee4bb8fb029c7a36173e1814fff40411d50ff1
iaconom821 <iaconom821@gmail.com>
2021-05-24 12:00:52 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/clients", to: "clients#index", as: "clients"
+  get "/clients/new", to: "clients#new", as: "new_client"
+  get "/clients/:id", to: "clients#show", as: "client"
+  post "/clients", to: "clients#create"
+  delete "/clients/:id", to: "clients#destroy"
+
+  get "/memberships", to: "memberships#index", as: "memberships"
+  get "/memberships/new", to: "memberships#new", as: "new_membership"
+  get "/memberships/:id", to: "memberships#show", as: "membership"
+  post "/memberships", to: "memberships#create"
+  delete "/memberships/:id", to: "memberships#destroy"
+
+  get "/gyms", to: "gyms#index", as: "gyms"
+  get "/gyms/new", to: "gyms#new", as: "new_gym"
+  get "/gyms/:id", to: "gyms#show", as: "gym"
+  post "/gyms", to: "gyms#create"
+  delete "/gyms/:id", to: "gyms#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Gym.destroy_all
+Client.destroy_all
+Membership.destroy_all
+
+5.times do 
+    Gym.create(name: Faker::Company.name, address: Faker::Address.full_address)
+end
+
+15.times do
+    Client.create(name: Faker::FunnyName.name, age: Faker::Number.within(range: 14..75))
+end
+
+
